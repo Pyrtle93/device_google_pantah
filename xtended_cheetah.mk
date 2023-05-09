f2cfee89659cabeb8d801c06072ac898bc44a932
xAndroidGangx <xxpyrtlexx93@gmail.com>
2023-05-09 18:41:38 -0400

    Xtended-ify

--- xtended_cheetah.mk
+++ xtended_cheetah.mk
@@ -1,22 +1,31 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Inherit some common Project-Xtended stuff.
 $(call inherit-product, vendor/xtended/config/common.mk)
+$(call inherit-product, vendor/gms/gms_full.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_cheetah.mk)
 
-# Project-Xtended Stuffz
-WITH_GAPPS := true
+# Project-Xtended Xtras
+WITH_GMS := true
 TARGET_FACE_UNLOCK_SUPPORTED := false
-EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_NGA := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Xtended Official Flags
+XTENDED_BUILD_TYPE := OFFICIAL
+XTENDED_BUILD_MAINTAINER := Pyrtle93
 
 # Parts
 $(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
-$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := xtended_cheetah
